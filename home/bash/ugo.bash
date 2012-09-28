642c1a07cafdc960c1aaf40cd02c273fed81682e
Ruben Nicolaides <ruben.nicolaides@webrunners.de>
2012-09-28 13:54:14 +0200

    fixed help

--- home/bash/ugo.bash
+++ home/bash/ugo.bash
@@ -21,7 +21,7 @@
 [[ $UGO_HOME == "" ]] && UGO_HOME="$HOME/.ugo"
 [[ $UGO_PROFILE == "" ]] && UGO_PROFILE=".profile"
 [[ $UGO_CONFDIR == "" ]] && UGO_CONFDIR=".config"
-[[ $UGO_TRASH == "" ]] && UGO_TRASH=$UGO_HOME/.Trash
+[[ $UGO_TRASH == "" ]] && UGO_TRASH="$UGO_HOME/.Trash"
 [[ -d "$UGO_HOME/$UGO_CONFDIR" ]] || mkdir -p "$UGO_HOME/$UGO_CONFDIR"
 [[ -d "$UGO_TRASH" ]] || mkdir -p "$UGO_TRASH"
 
@@ -45,18 +45,18 @@ commands ?(<optional>), options:
  set <project> ?<path>
  
 hooks:
-~/.ugo/_ugo_conf/
+$UGO_HOME/$UGO_CONFDIR/
  pre-make-hook
  post-make-hook
  
-~/.ugo/<project>/
+$UGO_HOME/<project>/
  pre-ugo-hook
  post-ugo-hook
  pre-delete-hook [only with delete --all]
  post-delete-hook [only with delete --all]
 
 bootscripts:
-~/.ugo/_ugo_conf/
+$UGO_HOME/$UGO_CONFDIR/
  boot-<name>
 
 HELO
