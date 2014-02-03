a31558a3d2e9037e5362df18627b57e0986a8b37
Ruben Nicolaides <ruben.nicolaides@webrunners.de>
2014-02-03 13:36:13 +0100

    extended examples

--- commandsets/example/commands.py
+++ commandsets/example/commands.py
@@ -3,22 +3,35 @@ COMMANDS = """
     "example": {
         "arguments": {
             "list": {
-                "list": "gu,fa, fub"
+                "list": "gu,fa, fub, nargs='?'"
             },
             "door": {
                 "list": "1, 2, 3",
-                "argparse_argument_args": "type=int, choices=range(1, 4), nargs='?'"
+                "argparse_argument_args": "type=int, choices=range(1, 4)"
             },
             "path_settings": {
                 "path": "BLA"
             },
             "path_system": {
-                "list": "7,8,9"
+                "list": "7,8,9",
+                "argparse_argument_args": "nargs='?'"
             },
             "--order": {
                 "list": "10,11,12",
-                "groups": "no_order"
+                "groups": "no_order",
+                "argparse_argument_args": "type=int, choices=range(10,13)"
             }
         }
-
-    }"""
+    },
+    "example2": {
+        "subcommands": {
+            "gimmepath": {
+                "arguments": {
+                    "path": {
+                        "call": "get_pathlist"
+                    }
+                }
+            }
+        }
+    }
+"""
---
+++ commandsets/example/utils.py
@@ -0,0 +1,1 @@
+from commandsets.base.utils import get_pathlist
